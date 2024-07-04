6aaafcece6b169fd507332158161a3fed539d14d
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-07-04 16:54:43 -0300

    feat: create reservation schema

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common'
 import { ReservationsService } from './reservations.service'
 import { ReservationsController } from './reservations.controller'
+import { ReservationRepository } from './reservations/repositories/reservation.repository'
+import { DatabaseModule } from '@app/common/database/database.module'
+import {
+  ReservationDocument,
+  ReservationSchema
+} from './reservations/schema/reservation.schema'
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema }
+    ])
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService]
+  providers: [ReservationsService, ReservationRepository] // inject in services
 })
 export class ReservationsModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigService } from '@nestjs/config';
-import { ConfigModule } from '@app/common/config/config.module';
+import { Module } from '@nestjs/common'
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose'
+import { ConfigService } from '@nestjs/config'
+import { ConfigModule } from '@app/common/config/config.module'
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
-        uri: config.get('MONGODB_URI'),
+        uri: config.get('MONGODB_URI')
       }),
-      inject: [ConfigService],
-    }),
-  ],
+      inject: [ConfigService]
+    })
+  ]
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models)
+  }
+}
