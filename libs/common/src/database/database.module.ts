84fa18f101894d573ae1b6e523c864f124778741
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-07-04 12:02:11 -0300

    feat: config database connection with mongodb

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://3965:3965@cluster0.ijjaozj.mongodb.net/sleepbnb',
+    ),
+  ],
+})
 export class DatabaseModule {}
