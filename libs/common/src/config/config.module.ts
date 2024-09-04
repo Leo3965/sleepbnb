5aaee17a6024cac195bbf68973d61ce863bd551e
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-09-04 10:52:19 -0300

    feat: add validation to .env variables

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config'
+import * as Joi from 'joi'
 
 @Module({
-  imports: [NestConfigModule.forRoot({})],
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required()
+      })
+    })
+  ],
   providers: [ConfigService],
-  exports: [NestConfigModule],
+  exports: [NestConfigModule]
 })
 export class ConfigModule {}
